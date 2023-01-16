948a66b1565928daf92c5d060173b8bd8be99bf7
shivammalviya205 <86865439+shivammalviya205@users.noreply.github.com>
2023-01-16 19:47:16 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': '521906d51amsh32d1c937f59c6c5p1c485cjsnd51115fab5da',
+    },
+  };
+
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '521906d51amsh32d1c937f59c6c5p1c485cjsnd51115fab5da',
+    },
+  };
+
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
